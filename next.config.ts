d21c23a40413f2a4b5f68611b51c57c0e23b1bca
sanjari alfian <sanjari.alfian.01@gmail.com>
2025-05-11 16:56:01 +0000

    perbaiki untuk Best Practices

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
